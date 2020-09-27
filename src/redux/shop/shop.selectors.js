c134e044a872f0ad84634e5c1d1e512d29b67bfa
Anish shetty k <47324253+Anishshettyk@users.noreply.github.com>
2020-09-27 09:47:13 +0530

    added links from home page to collections page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,6 @@
 import { createSelector } from "reselect";
 
 //to get the url parameter we pass the id equivalent to url name
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 
 const selectShop = (state) => state.shop;
 
@@ -16,9 +9,13 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key]) //converts the keys of an object into array format
+);
+
 export const selectCollection = (collectionUrlParams) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParams]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParams]
   );
