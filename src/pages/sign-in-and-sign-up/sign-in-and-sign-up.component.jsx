e21b83b8be1dfa74b95e95b3f4002d1016d41adf
Anish shetty k <47324253+Anishshettyk@users.noreply.github.com>
2020-09-28 17:21:39 +0530

    added styled component for sign-in-and-sign-up page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import "./sign-in-and-sign-up.styles.scss";
+import { SignInAndSignUpContainer } from "./sign-in-and-sign-up.styles";
 
 import SignIn from "./../../components/sign-in/sign-in.components";
 import SignUp from "./../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <SignInAndSignUpContainer>
     <SignIn />
     <SignUp />
-  </div>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInAndSignUpPage;
